24f3d96c692225a93b4b71443aca9390e184284a
TanKhoa1709 <nguyentankhoa170924@gmail.com>
2025-02-03 18:02:39 +0700

    i think it's done

--- lib/pages/weather_page.dart
+++ lib/pages/weather_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:weather_app/models/weather_model.dart';
+
+import '../services/weather_service.dart';
 
 class WeatherPage extends StatefulWidget {
   const WeatherPage({super.key});
@@ -8,8 +12,92 @@ class WeatherPage extends StatefulWidget {
 }
 
 class _WeatherPageState extends State<WeatherPage> {
+  // api key
+  final _weatherService = WeatherService('f49183a48b36a0f2d5524a04ade51482');
+  Weather? _weather;
+
+  // fetch weather
+  _fetchWeather() async {
+    // get the current city
+    String cityName = await _weatherService.getCurrentCity();
+
+    // fetch the weather for the current city
+    try {
+      final weather = await _weatherService.getWeather(cityName);
+      setState(() {
+        _weather = weather;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  // weather animations
+  String getWeatherAnimation(String? mainCondition) {
+    if (mainCondition == null) return 'assets/sunny.json'; // default
+
+    switch (mainCondition.toLowerCase()) {
+      case 'clouds':
+        return 'assets/cloud.json';
+      case 'sunny':
+        return 'assets/sunny.json';
+      case 'rain':
+        return 'assets/rainy.json';
+      case 'thunderstorm':
+        return 'assets/thunder.json';
+      case 'clear':
+        return 'assets/sunny.json';
+      default:
+        return 'assets/sunny.json';
+    }
+  }
+
+  // init state
+  @override
+  void initState() {
+    super.initState();
+
+    // fetch weather on startup
+    _fetchWeather();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.grey[700],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.location_on, size: 25, color: Colors.white),
+            // city name
+            Text(
+              _weather?.cityName.toUpperCase() ?? 'loading...',
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 100,
+            ),
+
+            //animation
+            Lottie.asset(getWeatherAnimation(_weather?.mainCondition)),
+
+            Text(
+              _weather?.mainCondition ?? 'loading...',
+              style: TextStyle(fontSize: 18),
+            ),
+
+            // temperature
+            Text(
+              '${_weather?.temperature.round()}°C',
+              style: TextStyle(fontSize: 18),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -24,26 +24,26 @@ class WeatherService {
   }
 
   Future<String> getCurrentCity() async {
-    //get permission from user
+    // get permission from user
     LocationPermission permission = await Geolocator.checkPermission();
     if (permission == LocationPermission.denied) {
       permission = await Geolocator.requestPermission();
     }
 
-    //fetch the current location
+    // fetch the current location
     Position position = await Geolocator.getCurrentPosition(
       locationSettings: LocationSettings( //this is the new thing
         accuracy: LocationAccuracy.high,
       )
     );
 
-    //convert the location into a list of placemark object
+    // convert the location into a list of placemark object
     List<Placemark> placemarks = await placemarkFromCoordinates(
       position.latitude,
       position.longitude,
     );
 
-    //extract the city name from the first placemark
+    // extract the city name from the first placemark
     String? city = placemarks[0].locality;
 
     return city ?? '';
